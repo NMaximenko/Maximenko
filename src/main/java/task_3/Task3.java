6689996de3ba337668f1b44d919f813d2798e035
Владелец <NMaximenko@aplana.com>
2019-04-07 16:15:22 +0300

    taaaask3-add zapyataya

--- src/main/java/task_3/Task3.java
+++ src/main/java/task_3/Task3.java
@@ -2,7 +2,7 @@ package task_3;
 
 public class Task3 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, world!");
     }
 
 }
