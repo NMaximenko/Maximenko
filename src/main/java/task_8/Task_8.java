d3d0094e7dca3550bb6ca78ec45bf406aa8fdffc
Владелец <NMaximenko@aplana.com>
2019-04-21 14:51:59 +0300

    task _8

---
+++ src/main/java/task_8/Task_8.java
@@ -0,0 +1,10 @@
+package task_8;
+
+import java.io.IOException;
+
+public class Task_8 {
+    public static void main(String[] args) throws IOException {
+        for (int i = 1; i <=50 ; i++)
+            System.out.println(i);
+    }
+}
