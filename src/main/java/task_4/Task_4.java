5d9d98b06ac0a92ebf3e9fb94554d648dc57d4ba
Владелец <NMaximenko@aplana.com>
2019-04-21 14:35:02 +0300

    task _4

---
+++ src/main/java/task_4/Task_4.java
@@ -0,0 +1,11 @@
+package task_4;
+
+import java.util.Scanner;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String bin=sc.next();
+        System.out.println(Integer.parseInt(bin,2));
+    }
+}
