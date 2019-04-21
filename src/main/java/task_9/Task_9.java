a0775fd1fc789e53a7d648d9b6c300a4dce55ee5
Владелец <NMaximenko@aplana.com>
2019-04-21 14:54:32 +0300

    task _9

---
+++ src/main/java/task_9/Task_9.java
@@ -0,0 +1,18 @@
+package task_9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task_9 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        int mass[] = new int[10];
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = Integer.parseInt(buf.readLine());
+        }
+        for (int i = 0; i < mass.length; i++) {
+            System.out.println(mass[i] * 2);
+        }
+    }
+}
