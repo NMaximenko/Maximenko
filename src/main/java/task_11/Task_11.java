8b14e82ab0e04b1634cd22f048436782ac6a9bd5
Владелец <NMaximenko@aplana.com>
2019-05-03 12:28:58 +0300

    task _10

---
+++ src/main/java/task_11/Task_11.java
@@ -0,0 +1,15 @@
+package task_11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task_11 {
+    public static void main(String[] args) throws IOException {
+        String res;
+        BufferedReader buf= new BufferedReader(new InputStreamReader(System.in));
+        res=buf.readLine();
+        System.out.println(res);
+
+    }
+}
