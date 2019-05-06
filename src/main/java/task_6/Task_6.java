1127579b527b2beb317ad09bf4d3c0cd0c180a0a
Владелец <NMaximenko@aplana.com>
2019-05-06 22:59:03 +0300

    task _6 измененная

--- src/main/java/task_6/Task_6.java
+++ src/main/java/task_6/Task_6.java
@@ -7,12 +7,13 @@ import java.io.InputStreamReader;
 public class Task_6 {
     public static void main(String[] args) throws IOException {
         BufferedReader buf=new BufferedReader(new InputStreamReader(System.in));
-        int x=Integer.parseInt(buf.readLine());
-        int y=Integer.parseInt(buf.readLine());
-        int z=Integer.parseInt(buf.readLine());
-        int avg=(x+y+z)/3;
+        double x=Integer.parseInt(buf.readLine());
+        double y=Integer.parseInt(buf.readLine());
+        double z=Integer.parseInt(buf.readLine());
+        double avg=(x+y+z)/3;
         System.out.println(avg);
-        if (avg%2==0&&avg/2>3)
+        int avg1=(int)avg/2;
+        if (avg>3)
             System.out.println("Программа выполнена корректно");
     }
 }
