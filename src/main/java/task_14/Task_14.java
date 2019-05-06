457d6e63717820146de46cf1cf0609c0a569a89d
Владелец <NMaximenko@aplana.com>
2019-05-06 23:26:11 +0300

    task_14 измененная

--- src/main/java/task_14/Task_14.java
+++ src/main/java/task_14/Task_14.java
@@ -5,18 +5,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Task_14 {
-    public static void main(String[] args) throws IOException {
-        String S;
-        int X;
-        double Y;
-        BufferedReader buf= new BufferedReader(new InputStreamReader(System.in));
-        S=buf.readLine();
-        X=Integer.parseInt(S);
-        Y=X;
+        public static void main (String[]args) throws IOException {
+            String S;
+            int X;
+            double Y;
+            BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+            S = buf.readLine();
+            //вариант 1) если введено нецелочисленное значение, можно бросить exception
+//        try {
+//            X = Integer.parseInt(S);
+//        }catch (NumberFormatException e){
+//            System.out.println("Веден строчный символ");
+//        }
+            //вариант 2. если введено нецелочисленное значение, то сохраняем для начала его в вещественном типе переменной и далее присваиваем в целочисленное
+            double D = (Double.parseDouble(S));
+            X = (int) D / 1;
+            Y = X;
+            System.out.println(S);
+            System.out.println(X);
+            System.out.println(Y);
 
-        System.out.println(S);
-        System.out.println(X);
-        System.out.println(Y);
-
+        }
     }
-}
