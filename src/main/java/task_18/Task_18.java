774b8bf153aade089143e4ce30a50226e518606c
Владелец <NMaximenko@aplana.com>
2019-05-06 23:32:10 +0300

    task_18 измененная

--- src/main/java/task_18/Task_18.java
+++ src/main/java/task_18/Task_18.java
@@ -9,22 +9,24 @@ public class Task_18 {
         File file = new File(point);
         FileInputStream fileInputStream=new FileInputStream(file);
         String out="";
+        int count=0;
         buf=new BufferedReader(new InputStreamReader(fileInputStream));
         while ((out=buf.readLine())!=null) {
+            count++;
             System.out.println(out);
         }buf.close();
         String in="";
         FileWriter fileWriter=new FileWriter(file);
         BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);
         BufferedReader buffer=new BufferedReader(new InputStreamReader(System.in));
-        while (true){
-            if ((in=buffer.readLine()).equals("stop!"))
-                break;
+        while (count!=0){
+            in=buffer.readLine();
             bufferedWriter.write(in);
             bufferedWriter.newLine();
+            count--;
         }
         buffer.close();
         bufferedWriter.close();
 
     }}
-}
+
